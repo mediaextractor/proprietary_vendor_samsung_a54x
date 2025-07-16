c92d4e324f7f00cf047d2d56c48cb0e0193e4bcf
Ksawlii <ksawery.blaszczak@proton.me>
2025-07-16 18:19:25 +0200

    s5e8825: scripts: UGHHHHHHHHHHHHHHHH
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,9 +1,10 @@
-FILES=( "boot.img" "dtbo.img" "vbmeta.img" "vendor_boot.img" )
+FILES=( "boot.img.lz4" "dtbo.img.lz4" "vbmeta.img.lz4" "vendor_boot.img.lz4" )
 
 for f in "${FILES[@]}"; do
     tar xvf ${AP_TAR} ${f}
-    lz4 -d ${f}.lz4 >/dev/null 2>&1
+    lz4 -d ${f} >/dev/null 2>&1
 done
 
-tar cvf ${LATEST_SHORTVERSION}_kernel.tar *.img
-rm *.img
+FILES="boot.img dtbo.img vbmeta.img vendor_boot.img"
+tar cvf ${LATEST_SHORTVERSION}_kernel.tar $FILES
+rm -f $FILES
