ccd8e90b8dd7971cbe9d005dafcb925c867c51db
Ksawlii <ksawery.blaszczak@proton.me>
2025-07-16 17:56:56 +0200

    s5e8825: scripts: Extract vendor and pack it in to a zip
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -7,3 +7,17 @@ mv -f super_raw.img super.img
 ./tools/lpunpack -p vendor super.img .
 rm -f super.img
 [[ -e vendor.img ]] && zip ${LATEST_SHORTVERSION}_vendor.zip vendor.img
+
+mkdir -p vendor vendor_mount
+sudo mount vendor.img vendor_mount
+
+# https://github.com/salvogiangri/UN1CA/blob/fifteen/scripts/extract_fw.sh#L135-L136
+sudo cp -fa -T vendor_mount vendor
+sudo chown -hR "$(whoami):$(whoami)" vendor
+sudo umount vendor_mount
+
+cp -rfa vendor/firmware/* vendor/firmware/${MODEL}
+cp -rfa vendor/tee/* vendor/tee/${MODEL}
+
+zip -r0 --store ${LATEST_SHORTVERSION}_vendor-extracted.zip vendor
+rm -rf vendor vendor_mount
