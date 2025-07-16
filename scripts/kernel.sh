115d07dd10acc43837028e46a316deb060cd11e8
Ksawlii <ksawery.blaszczak@proton.me>
2025-07-16 18:10:51 +0200

    s5e8825: scripts: Fucking fixup
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,8 +1,8 @@
-FILES=( "boot.img.lz4" "dtbo.img.lz4" "vbmeta.img.lz4" "vendor_boot.img.lz4" )
+FILES=( "boot.img" "dtbo.img" "vbmeta.img" "vendor_boot.img" )
 
 for f in "${FILES[@]}"; do
     tar xvf ${AP_TAR} ${f}
-    lz4 -d ${f}
+    lz4 -d ${f}.lz4 >/dev/null 2>&1
 done
 
 tar cvf ${LATEST_SHORTVERSION}_kernel.tar *.img
