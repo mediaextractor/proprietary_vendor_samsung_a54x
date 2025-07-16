4f78f51ae749989f83267bdd4e144ca4c59870f9
Ksawlii <ksawery.blaszczak@proton.me>
2025-07-16 17:47:14 +0200

    s5e8825: scripts: Decompress kernel images before packing

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -2,7 +2,8 @@ FILES=( "boot.img.lz4" "dtbo.img.lz4" "vbmeta.img.lz4" "vendor_boot.img.lz4" )
 
 for f in "${FILES[@]}"; do
     tar xvf ${AP_TAR} ${f}
+    lz4 -d ${f}
 done
 
-tar cvf ${LATEST_SHORTVERSION}_kernel.tar *.lz4
-rm *.lz4
+tar cvf ${LATEST_SHORTVERSION}_kernel.tar *.img
+rm *.img
