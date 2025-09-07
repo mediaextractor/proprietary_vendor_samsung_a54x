f90b2ee88672311a4a127c8b1a1b7830fe341e76
Majaahh <maja.blaszczak@proton.me>
2025-09-07 23:09:22 +0200

    s5e8825: scripts: Get libsn100u_fw.so
    * SC-53C has a diff nfc chip
    
    Change-Id: Ie415ac97bc220f80543d6f69d2c6305e35aa5e9f
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/proprietary-files.sh
+++ scripts/proprietary-files.sh
@@ -4,6 +4,7 @@ audio_blobs=( "APDV_AUDIO_SLSI.bin" "AP_AUDIO_SLSI.bin" "calliope_sram.bin" "vts
 sudo grep -q "m34" "../mount/build.prop" && audio_blobs=( "calliope_sram.bin" "vts.bin" )
 
 fw_blobs=( "NPU.bin"  "mfc_fw.bin" "os.checked.bin" )
+[[ -f "nfc/libsn100u_fw.so" ]] && fw_blobs+=( "libsn100u_fw.so" )
 
 #####################################################################################
 
