3e1f47e45415647b6bf2159f9234e110afcd4551
Majaahh <maja.blaszczak@proton.me>
2025-09-08 23:33:17 +0200

    s5e8825: scripts: Fix getting kernel images
    Change-Id: I8720206a49be67122788479be94a20b2e5fe734b
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -2,9 +2,8 @@ FILES=( "boot.img.lz4" "dtbo.img.lz4" "vbmeta.img.lz4" "vendor_boot.img.lz4" )
 
 for f in "${FILES[@]}"; do
     tar xvf ${AP_TAR} ${f}
-    lz4 -d ${f} >/dev/null 2>&1
 done
 
-FILES="boot.img dtbo.img vbmeta.img vendor_boot.img"
-tar cvf ${LATEST_SHORTVERSION}_kernel.tar $FILES
+FILES="boot dtbo vbmeta vendor_boot"
+tar cvf ${LATEST_SHORTVERSION}_kernel.tar ${FILES}.img.lz4
 rm -f $FILES
