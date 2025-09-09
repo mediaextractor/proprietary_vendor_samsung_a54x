c5ed589c8732556fbbb51e88d363d43456d77e3e
Majaahh <maja.blaszczak@proton.me>
2025-09-09 15:14:56 +0200

    s5e8825: scripts: Fix libsn100u_fw path
    Change-Id: Ic8d642ed3423f8af1fee844df8f0908470cb0e21
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/proprietary-files.sh
+++ scripts/proprietary-files.sh
@@ -10,7 +10,7 @@ audio_blobs=( "APDV_AUDIO_SLSI.bin" "AP_AUDIO_SLSI.bin" "calliope_sram.bin" "vts
 sudo grep -q "m34" "../mount/build.prop" && audio_blobs=( "calliope_sram.bin" "vts.bin" )
 
 fw_blobs=( "NPU.bin" "mfc_fw.bin" "os.checked.bin" )
-[[ -f "nfc/libsn100u_fw.so" ]] && fw_blobs+=( "libsn100u_fw.so" )
+[[ -f "nfc/libsn100u_fw.so" ]] && fw_blobs+=( "nfc/libsn100u_fw.so" )
 
 write_section() {
     local header="$1"; shift
