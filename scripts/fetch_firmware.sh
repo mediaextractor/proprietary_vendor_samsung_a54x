e60c10b970307ade2ba901318b935ffb4da22f97
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-09-09 11:21:07 +0200

    s5e8825: scripts: Work around random FUS failure
    Change-Id: Ia88f18ff4274d393824f38af24473025c9438210
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/fetch_firmware.sh
+++ scripts/fetch_firmware.sh
@@ -11,12 +11,27 @@ pip3 install git+https://github.com/ananjaser1211/samloader.git
 mkdir -p "${MODEL}_${CSC}"
 
 # Get firmware
-samloader -m ${MODEL} -r ${CSC} ${ARGS} download -O "${MODEL}_${CSC}" || exit 1
+C=1
+while true; do
+    samloader -m ${MODEL} -r ${CSC} ${ARGS} download -O "${MODEL}_${CSC}"
+    ZIP_FILE="$(find "${MODEL}_${CSC}" -name "*.zip" | sort -r | head -n 1)"
+    if [[ ! "$ZIP_FILE" ]] || [[ ! -f "$ZIP_FILE" ]]; then
+        if [[ "$C" -gt 10 ]]; then
+            exit 1
+        fi
+        echo -e "[Attempt: $C] Download failed, retrying in 5 seconds..."
+        sleep 5
+        ((C++))
+    else
+        break
+    fi
+done
+
+(
 cd "${MODEL}_${CSC}"
 unzip *".zip" 
-
-# Cleanup
 rm -rf *".zip"
-cd ".."
+)
+
 deactivate
 rm -rf "py_env"
