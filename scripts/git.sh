cd233ff3deba2a7fc7888aa172dd8fcc01861a5f
Majaahh <maja.blaszczak@proton.me>
2025-09-13 14:14:31 +0200

    s5e8825: Delete the tag and release if they exist
    Change-Id: I5089f0bd6aaccc9853df7fa28b7c4a369b562402
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,6 +5,18 @@ git config --local user.name "github-actions[bot]"
 # Pull
 git pull origin ${GHR} --ff-only
 
+# Tag and release
+TAG="${LATEST_SHORTVERSION}_${CSC}_${OMC}"
+if gh release view "$TAG" &>/dev/null; then
+    gh release delete "$TAG" -y
+fi
+if git ls-remote --tags origin | grep -q "refs/tags/$TAG"; then
+    git push origin --delete "$TAG"
+fi
+if git rev-parse -q --verify "refs/tags/$TAG" >/dev/null; then
+    git tag -d "$TAG"
+fi
+
 # Commit
 echo "${LATEST_VERSION}" >> "current.${MODEL}_${CSC}_${OMC}"
 git add current.${MODEL}_${CSC}_${OMC}
