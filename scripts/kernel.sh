ac03a78191fff98feb0052a5197742950328ad53
Majaahh <maja.blaszczak@proton.me>
2025-10-03 12:36:09 +0200

    s5e8825: scripts: kernel: pro fixing this one
    Change-Id: I7b5f08e4bebdb2dd491deeed57677ed738b76c28
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,9 +1,5 @@
-FILES=( "boot.img.lz4" "dtbo.img.lz4" "vbmeta.img.lz4" "vendor_boot.img.lz4" )
+FILES="boot.img.lz4 dtbo.img.lz4 vendor_boot.img.lz4"
 
-for f in "${FILES[@]}"; do
-    tar xvf ${AP_TAR} ${f}
-done
-
-FILES="boot dtbo vbmeta vendor_boot"
-tar cvf ${LATEST_SHORTVERSION}_kernel.tar ${FILES}.img.lz4
+tar xvf ${AP_TAR} ${FILES}
+tar cvf ${LATEST_SHORTVERSION}_kernel.tar ${FILES}
 rm -f $FILES
